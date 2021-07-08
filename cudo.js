03fa29d3cb5a36ab851cb231f7e068c721bef52b
Veselin Vlasakiev <veselin.vlasakiev@limechain.tech>
2021-07-08 16:52:22 +0300

    improve init cmd

--- cudo.js
+++ cudo.js
@@ -7,13 +7,20 @@ const {
 
 require('yargs')
     .scriptName("cudo")
+    .version()
     .usage('$0 <cmd> [args]')
     .command('init [contractname]', 'init smart contract template', (yargs) => {
         yargs.positional('contractname', {
-            type: 'string',
-            default: 'contract1',
-            describe: 'smart contract name'
-        })
+                type: 'string',
+                default: 'contract1',
+                describe: 'smart contract name'
+            })
+            .option('dir', {
+                alias: 'd',
+                type: 'string',
+                default: '.',
+                description: 'smart contracs directory'
+            })
     }, initCmd)
     .command('compile [contractname]', 'compile smart contract', (yargs) => {
         yargs.positional('contractname', {
