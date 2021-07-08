5181d41aedad4faa2fb452fb887119ec24cb6ebb
Veselin Vlasakiev <veselin.vlasakiev@limechain.tech>
2021-07-08 13:38:38 +0300

    add cmd module

---
+++ cmd/index.js
@@ -0,0 +1,10 @@
+const {
+    initCmd
+} = require("./init");
+
+const {
+    compileCmd
+} = require("./compile");
+
+module.exports.initCmd = initCmd;
+module.exports.compileCmd = compileCmd;
--- cudo.js
+++ cudo.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
 
 const {
-    execSync
-} = require("child_process");
-
-const path = require('path');
-const fs = require('fs')
+    initCmd,
+    compileCmd
+} = require("./cmd");
 
 require('yargs')
     .scriptName("cudo")
@@ -16,35 +14,13 @@ require('yargs')
             default: 'contract1',
             describe: 'smart contract name'
         })
-    }, function(argv) {
-        if (fs.existsSync(argv.contractname)) {
-            console.log(`${argv.contractname} exists`);
-            return
-        }
-
-        execSync(`cp -r template ${argv.contractname}`);
-        console.log('Done.');
-    })
+    }, initCmd)
     .command('compile [contractname]', 'compile smart contract', (yargs) => {
         yargs.positional('contractname', {
             type: 'string',
             default: 'contract1',
             describe: 'smart contract name'
         })
-    }, function(argv) {
-        if (!fs.existsSync(argv.contractname)) {
-            console.log(`${argv.contractname} does not exist`);
-            return
-        }
-        // temporary solution before uploading our dockerimage to the Hub
-        try {
-            execSync("docker inspect cudo/rust-wasm").toString('utf-8');
-        } catch (e) {
-            execSync("docker build -f rust-wasm.Dockerfile --tag cudo/rust-wasm .").toString('utf-8');
-        }
-
-        execSync(`docker run --rm -v "${process.env.PWD}/${argv.contractname}":/code --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry cudo/rust-wasm`).toString('utf-8');
-        console.log("Compiled.");
-    })
+    }, compileCmd)
     .help()
     .argv
