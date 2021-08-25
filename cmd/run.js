be96ab806242d9432d111fea3e1d686e43fd9ac0
Veselin Vlasakiev <veselin.vlasakiev@limechain.tech>
2021-08-25 15:46:02 +0300

    Add gitignore and fix func name

--- cmd/run.js
+++ cmd/run.js
@@ -2,20 +2,16 @@ const process = require('process');
 
 const path = require('path')
 
-//consit SCRIPT_FILE = path.join(process.cwd(), 'script-deploy.js');
-
 const fs = require('fs');
 const vm = require('vm');
-
 
 const {getContractFactory} = require('./lib/contract');
 
 global.getContractFactory = getContractFactory;
 
-async function deployCmd(argv) {
+async function runCmd(argv) {
 
-
-    const ds = new vm.Script(fs.readFileSync(SCRIPT_FILE));
+    const ds = new vm.Script(fs.readFileSync(argv.scriptfile));
     await ds.runInThisContext();
 }
 
