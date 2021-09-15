78cf41beb40fb0ab6bd47203655882e0962b3fb2
infra1515 <lyubomir.yonchev@limechain.tech>
2021-09-15 14:19:48 +0300

    [CUDOS-198] More work done on compile

--- cmd/compile.js
+++ cmd/compile.js
@@ -6,18 +6,19 @@ const {
 const path = require('path');
 
 function compileCmd(argv) {
-    let optcmd = `docker run --rm -v "${process.env.PWD}/contracts/${argv.contractname}":/code  --mount type=volume,source="${argv.contractname}_cache",target=/code/target  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry cosmwasm/rust-optimizer:0.11.3`
+    let optcmd = `docker run --rm -v "${process.env.PWD}/contracts/alpha/src/${argv.contractname}":/code  --mount type=volume,source="${argv.contractname}_cache",target=/code/target  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry cosmwasm/rust-optimizer:0.11.3`
 
+    console.log(optcmd);
     if (argv.contractname === undefined || argv.contractname === '') {
         console.error("You must specify a contract to compile! Execute cudo compile --help for more info.");
         return;
     }
-    if (!fs.existsSync(`${process.env.PWD}/contracts/${argv.contractname}`)) {
+    if (!fs.existsSync(`${process.env.PWD}contracts/alpha/src/${argv.contractname}`)) {
         console.log(`Contract with name: ${argv.contractname} does not exist. Execute cudo compile --help for more info.`);
         return
     }
     console.log('compiling...');
-    const r = execSyncCmd(optcmd);
+    const r = execSyncCmd(optcmd, { stdio: 'inherit' });
     console.log(String(r));
 }
 
--- cudo.js
+++ cudo.js
@@ -29,11 +29,18 @@ async function main() {
                     description: 'project directory'
                 })
         }, initCmd)
-        .command('compile [contractname]', 'compile smart contract', (yargs) => {
+        .command('compile [contractname]', 'Compiles one or more smart contracts. Example usage: \ncudo compile contractName\ncudo compile contractName1 contractName2 contractName3', (yargs) => {
             yargs.positional('contractname', {
                 type: 'string',
                 describe: 'The smart contract to compile name',
             })
+
+            // yargs.option("contracts", {
+            //     type: 'string',
+            //     array: true,
+            //     describe: 'Array of contracts to compile.\nEx: cudo compile --many contract1 contract2 contract3'
+            // })
+
         }, compileCmd)
         .command(nodeCmd)
         .command('run [scriptFilePath]', 'run script', (yargs) => {
