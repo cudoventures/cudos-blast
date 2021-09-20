31a22b0839b870776ba866f1bcbc6990962e2379
infra1515 <lyubomir.yonchev@limechain.tech>
2021-09-20 14:24:07 +0300

    [CUDOS-199] unused arguments removed from node command

--- cmd/node.js
+++ cmd/node.js
@@ -11,20 +11,20 @@ const {
 const dockerComposeFile = path.join(__dirname, '..', 'cudos-node.yaml');
 const cudosNodeHomeDir = './cudos_data/node';
 
-const startNode = async function (argv) {
+const startNode = async function () {
     execSyncCmd(`docker-compose -f ${dockerComposeFile} up -d`, { stdio: 'inherit' });
 };
 
-const stopNode = function (argv) {
+const stopNode = function () {
     execSyncCmd(`docker-compose -f ${dockerComposeFile} down`, { stdio: 'inherit' });
 };
 
-const statusNode = function (argv) {
+const statusNode = function () {
     let nStatus = execSyncCmd(`docker-compose -f ${dockerComposeFile} exec -T cudos-node cudos-noded --home ${cudosNodeHomeDir} status`, { stdio: 'inherit' });
     console.log(nStatus.toString());
 };
 
-const keysNode = async function (argv) {
+const keysNode = async function () {
     const keys = execSyncCmd(`docker-compose -f ${dockerComposeFile} exec -T cudos-node cudos-noded --home ${cudosNodeHomeDir} keys list  --output json`, { stdio: 'inherit' });
     console.log(JSON.parse(keys));
 
@@ -39,16 +39,14 @@ exports.command = 'node';
 exports.describe = 'manage cudo local node';
 
 exports.builder = (yargs) => {
-    yargs.command('start', 'start node', (yargs) => { }, startNode)
-        .command('stop', 'stopping node', (yargs) => { }, stopNode)
-        .command('status', 'check node status', (yargs) => { }, statusNode)
-        .command('keys', 'list keys', (yargs) => { }, keysNode)
-        .command('getpriv [user]', 'get privkey', (yargs) => {
+    yargs.command('start', 'start node', () => { }, startNode)
+        .command('stop', 'stopping node', () => { }, stopNode)
+        .command('status', 'check node status', () => { }, statusNode)
+        .command('keys', 'list keys', () => { }, keysNode)
+        .command('getpriv [user]', 'get privkey', () => {
             yargs.positional('user', {
                 type: 'string',
                 describe: 'account name',
             })
         }, _getPrivKey);
 };
-
-exports.handler = function (argv) { }
