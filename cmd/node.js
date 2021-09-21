c65b7f9d822d93b90536deb9d60629dd1a13bced
infra1515 <lyubomir.yonchev@limechain.tech>
2021-09-21 11:12:43 +0300

    [CUDOS-199] Docker file - static version of the base image;  Cmd and yaml command changed to chain serve;  Pass additional config to docker - env file with configuration;

--- cmd/node.js
+++ cmd/node.js
@@ -8,24 +8,23 @@ const {
     getPrivKey
 } = require('./lib/node');
 
-const dockerComposeFile = path.join(__dirname, '..', 'cudos-node.yaml');
+const dockerComposeFile = path.join(__dirname, '..', 'docker-compose.yaml');
 const cudosNodeHomeDir = './cudos_data/node';
 
 const startNode = async function () {
-    execSyncCmd(`docker-compose -f ${dockerComposeFile} up -d`, { stdio: 'inherit' });
+    execSyncCmd(`docker-compose --env-file=.docker_env -f ${dockerComposeFile} up -d`, { stdio: 'inherit' });
 };
 
 const stopNode = function () {
-    execSyncCmd(`docker-compose -f ${dockerComposeFile} down`, { stdio: 'inherit' });
+    execSyncCmd(`docker-compose --env-file=.docker_env -f ${dockerComposeFile} down`, { stdio: 'inherit' });
 };
 
 const statusNode = function () {
-    let nStatus = execSyncCmd(`docker-compose -f ${dockerComposeFile} exec -T cudos-node cudos-noded --home ${cudosNodeHomeDir} status`, { stdio: 'inherit' });
-    console.log(nStatus.toString());
+    execSyncCmd(`docker-compose --env-file=.docker_env -f ${dockerComposeFile} exec -T cudos-node cudos-noded --home ${cudosNodeHomeDir} status`, { stdio: 'inherit' });
 };
 
 const keysNode = async function () {
-    const keys = execSyncCmd(`docker-compose -f ${dockerComposeFile} exec -T cudos-node cudos-noded --home ${cudosNodeHomeDir} keys list  --output json`, { stdio: 'inherit' });
+    const keys = execSyncCmd(`docker-compose --env-file=.docker_env -f ${dockerComposeFile} exec -T cudos-node cudos-noded --home ${cudosNodeHomeDir} keys list  --output json`, { stdio: 'inherit' });
     console.log(JSON.parse(keys));
 
 };
