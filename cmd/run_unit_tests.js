2ffd97128eed0d647af8c4bde7a1ff4f2962f427
infra1515 <lyubomir.yonchev@limechain.tech>
2021-09-24 17:47:35 +0300

    [CUDOS-200] Problems mapped

--- cmd/run_unit_tests.js
+++ cmd/run_unit_tests.js
@@ -6,7 +6,9 @@ const path = require('path');
 
 function runUnitTests() {
     try {
-        let cmd = `docker run --rm --user "$(id -u)":"$(id -g)" -v "${path.resolve('.')}":/usr/src/cudos-cli -w /usr/src/cudos-cli rust:1.55-slim-buster  cargo test --lib`
+        // TODO: the slimbuster img is 604 mb, can we reuse the rust-optimizer to call the test? - So far could not make it work
+        // on each test run the docker is downloading the packages again, how can we cache them?
+        let cmd = `docker run --rm --user "$(id -u)":"$(id -g)" -v "${path.resolve('.')}":/usr/src/cudos-cli -w /usr/src/cudos-cli rust:1.55-slim-buster cargo test --lib`
         execSyncCmd(cmd, { stdio: 'inherit' });
     }
     catch (ex) {
