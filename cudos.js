6e906d3060222a036844b61d95a964d44439d2d2
ealeksandrov-LimeChain <92088769+ealeksandrov-LimeChain@users.noreply.github.com>
2021-10-11 13:48:10 +0300

    Enable automatic --help

--- cudos.js
+++ cudos.js
@@ -55,7 +55,7 @@ async function main() {
         .demandCommand(1, "No command specified!") // user must specify atleast one command 
         .recommendCommands()
         .strictCommands() // checks if the command is specified, if its not - user friendly error
-        .showHelpOnFail(false, 'Specify --help for available options') // does not show help automatically, just tells the user that he can call --help for available options
+        .showHelpOnFail(true) // show help automatically
         .help()
         .argv
 }
