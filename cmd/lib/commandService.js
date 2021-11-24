01c891a89516b0d9d95c5ec2f588989fc01d1d17
Ventsislav Tsochev <ventsi@limechain.tech>
2021-11-24 13:21:10 +0200

    init-root.sh modified, dockerComposeCmd updated

--- cmd/lib/commandService.js
+++ cmd/lib/commandService.js
@@ -14,54 +14,54 @@ const optimizerVer = '0.12.3';
 
 const cudosNodeHomeDir = './cudos_data/node';
 
-const dockerComposeCmd = `docker-compose --env-file=${getDockerEnvFile()} -f ${getDockerComposeFile()} `;
-
+const dockerComposeCmd = `docker-compose -f docker-compose-start.yaml -f docker-compose-init.yaml`;
+// const dockerComposeCmd = `docker-compose --env-file=${getDockerEnvFile()} -f ${getDockerComposeFile()} `; // OLD
 const nodeCmd = `exec -T cudos-node cudos-noded --home ${cudosNodeHomeDir} `;
 const starportCmd = `exec -T cudos-node starport --home ${cudosNodeHomeDir} `;
 
-const doDocker = function(cmd) {
+const doDocker = function (cmd) {
     spawnSync(cmd, {
         stdio: 'inherit',
         shell: true
     });
-}
+};
 
-const execute = function(arg) {
-    let cmd = dockerComposeCmd + arg
+const execute = function (arg) {
+    let cmd = dockerComposeCmd + arg;
     doDocker(cmd);
-}
+};
 
-const executeNode = function(arg) {
+const executeNode = function (arg) {
     execute(nodeCmd + arg);
-}
+};
 
-const executeStarport = function(arg) {
+const executeStarport = function (arg) {
     execute(starportCmd + arg);
-}
+};
 
-const stopNode = function() {
-    execute('down');
-}
+const stopNode = function () {
+    execute(' down');
+};
 
-const startNode = function(inBackground) {
+const startNode = function (inBackground) {
     if (inBackground) {
-        execute('up -d');
+        execute(' up --build -d');
     } else {
-        execute('up');
+        execute(' up --build');
     }
-}
+};
 
-const keysNode = function() {
+const keysNode = function () {
     executeNode('keys list  --output json');
-}
+};
 
-const fundAccount = function(address, tokens) {
+const fundAccount = function (address, tokens) {
     executeStarport(`chain faucet ${address} ${tokens}`);
-}
+};
 
-const compile = function() {
+const compile = function () {
     const projectRootPath = getProjectRootPath();
-    const compileCmd = `docker run --rm -v "${projectRootPath}":/code  --mount type=volume,source="contracts_cache",target=/code/target  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry cosmwasm/workspace-optimizer:${optimizerVer}`
+    const compileCmd = `docker run --rm -v "${projectRootPath}":/code  --mount type=volume,source="contracts_cache",target=/code/target  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry cosmwasm/workspace-optimizer:${optimizerVer}`;
 
     console.log(`${projectRootPath}/contracts`);
     if (!fs.existsSync(`${projectRootPath}/contracts`)) {
@@ -69,7 +69,7 @@ const compile = function() {
     }
     doDocker(compileCmd);
 
-}
+};
 
 module.exports = {
     stopNode: stopNode,
@@ -77,4 +77,4 @@ module.exports = {
     keysNode: keysNode,
     fundAccount: fundAccount,
     compile: compile
-}
+};
