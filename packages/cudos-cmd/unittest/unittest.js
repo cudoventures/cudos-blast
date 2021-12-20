e6ac38a48ab28fd423d0c4ce7907755d939bbe19
ealeksandrov-LimeChain <92088769+ealeksandrov-LimeChain@users.noreply.github.com>
2021-12-20 18:00:55 +0200

    small cosmetic changes

--- packages/cudos-cmd/unittest/unittest.js
+++ packages/cudos-cmd/unittest/unittest.js
@@ -1,12 +1,11 @@
-const { executeRun } = require('../../cudos-utilities/run-docker-commands')
-
 const path = require('path')
+
+const { executeRun } = require('../../cudos-utilities/run-docker-commands')
 
 function runUnitTests() {
   try {
     // TODO: the slimbuster img is 604 mb, can we reuse the rust-optimizer to call the test? - So far could not make it
     // work on each test run the docker is downloading the packages again, how can we cache them?
-    // At the time of this comment execSync(cmd, { stdio: 'inherit' }) was used instead of spawnSync...
     const cmd = `-v "${path.resolve('.')}":/usr/src/cudos-cli ` +
         '-w /usr/src/cudos-cli rust:1.55-slim-buster cargo test --lib'
     executeRun(cmd)
