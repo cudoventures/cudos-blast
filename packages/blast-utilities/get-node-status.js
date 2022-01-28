e74bf0c3fa162d65b9c4383decfa4d60404ed98d
ealeksandrov-LimeChain <92088769+ealeksandrov-LimeChain@users.noreply.github.com>
2022-01-28 11:44:20 +0200

    refactor node status logging

--- packages/blast-utilities/get-node-status.js
+++ packages/blast-utilities/get-node-status.js
@@ -28,36 +28,30 @@ async function getNodeStatusByUrl(url) {
     if (response.status === 200) {
       if (response.data.result && response.data.result.node_info) {
         // node is up and running
-        const nodeStatus = {}
-        nodeStatus.isConnected = true
-        nodeStatus.info = 'Node is online.\n' + 'Node id: ' + response.data.result.node_info.id + '\nNetwork: ' +
-          response.data.result.node_info.network
-        return nodeStatus
+        return nodeInfo(true, 'Node id: ' + response.data.result.node_info.id + '\nNetwork: ' +
+          response.data.result.node_info.network)
       }
       // probably connected to something other than node
-      return nodeInfoOffline(response.status, 'Missing data from node response. Are you sure you connected a node?')
+      return nodeInfo(false, 'Missing data from node response. Are you sure you connected a node?')
     }
     // status is not 200
-    return nodeInfoOffline(response.status, 'Unusual node response. Status code should be 200.')
+    return nodeInfo(false, 'Status code: ' + response.status + '\nUnusual node response. Status code should be 200.')
   } catch (error) {
     if (!error.isAxiosError) {
       throw error
     }
     // get status code either from axios response or axios error
     if (typeof error.response !== 'undefined') {
-      return nodeInfoOffline(error.response.status)
+      return nodeInfo(false, 'Status code: ' + error.response.status)
     }
-    return nodeInfoOffline(error.code)
+    return nodeInfo(false, 'Status code: ' + error.code)
   }
 }
 
-function nodeInfoOffline(statusCode, additionalInfo) {
+function nodeInfo(isOnline, additionalInfo) {
   const nodeStatus = {}
-  nodeStatus.isConnected = false
-  nodeStatus.info = 'Node is offline.\nStatus code: ' + statusCode
-  if (additionalInfo) {
-    nodeStatus.info += '\n' + additionalInfo
-  }
+  nodeStatus.isConnected = isOnline
+  nodeStatus.info = (isOnline ? 'Node is online.\n' : 'Node is offline.\n') + additionalInfo
   return nodeStatus
 }
 
