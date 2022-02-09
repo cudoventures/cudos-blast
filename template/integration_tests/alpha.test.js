8e193c85b7d9d54bbd121a5257b5d6bb46153cd9
Tugay Emin <tgntrr@gmail.com>
2022-02-09 18:00:52 +0200

    CUDOS-608 adjust template integration tests

--- template/integration_tests/alpha.test.js
+++ template/integration_tests/alpha.test.js
@@ -1,17 +1,19 @@
-describe('alpha test', async function () {
-  const alphaContract = await getContractFactory('alpha', {
-    count: 13
+const expect = require('chai').expect
+
+describe('alpha test', function () {
+  it('should get count', async function () {
+    await setClient(null, null)
+
+    const alphaContract = await getContractFactory('alpha', {
+      count: 13
+    })
+  
+    await alphaContract.deploy()
+
+    const count = await alphaContract.querySmart({
+      get_count: {}
+    })
+
+    expect(count).eql({ count: 13 })
   })
-
-  const contractAddress = await alphaContract.deploy()
-
-  const _alphaContract = await getContractFromAddress(contractAddress)
-
-  const {
-    count
-  } = await alphaContract.querySmart({
-    get_count: {}
-  })
-
-  expect(count).to.equal(14)
 })
