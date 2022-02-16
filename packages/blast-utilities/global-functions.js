9952602ca8a616bdbe9a1b0376cd325e098baa60
Tugay Emin <tgntrr@gmail.com>
2022-02-16 11:30:02 +0200

    fix lint errors

--- packages/blast-utilities/global-functions.js
+++ packages/blast-utilities/global-functions.js
@@ -1,7 +1,7 @@
 const path = require('path')
-const BlastError = require("./blast-error")
-const { CudosContract } = require("./contract-utils")
-const { getProjectRootPath } = require("./package-info")
+const BlastError = require('./blast-error')
+const { CudosContract } = require('./contract-utils')
+const { getProjectRootPath } = require('./package-info')
 const {
   DirectSecp256k1HdWallet,
   SigningCosmWasmClient,
@@ -10,25 +10,25 @@ const {
 const {
   getNetworkUrl,
   getAddressPrefix
-} = require("./config-utils")
+} = require('./config-utils')
 
 const accounts = getAccounts()
 const networkUrl = getNetworkUrl()
 const addressPrefix = getAddressPrefix()
 
-global.getSigners = async function () {
-  let signers = []
-  for (acc of accounts) {
+global.getSigners = async function() {
+  const signers = []
+  for (const acc of accounts) {
     signers.push(await getSigner(acc))
   }
   return signers
 }
 
-global.getContractFactory = async function (contractName) {
+global.getContractFactory = async function(contractName) {
   return new CudosContract(contractName, await getSigner(accounts[0]))
 }
 
-global.getContractFromAddress = async function (contractAddress, owner = null) {
+global.getContractFromAddress = async function(contractAddress, owner = null) {
   const contractInfo = await getContractInfo(contractAddress)
   const creatorAccount = accounts.find(acc => acc.address === contractInfo.creator)
 
